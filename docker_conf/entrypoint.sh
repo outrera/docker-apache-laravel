6ab9a55b2d534f8fa19483f3c2a78b38ec1d5507
Sergei ZH <sergey95aeses@gmail.com>
2017-09-03 11:01:18 +0200

    Automatically prepare laravel and static files if production

--- docker_conf/entrypoint.sh
+++ docker_conf/entrypoint.sh
@@ -10,6 +10,18 @@ fi
 
 /bin/chown www-data:www-data -R "$SERVER_ROOT/storage" "$SERVER_ROOT/bootstrap/cache"
 
+# Prepare laravel and static files
+if [ $RUN_MODE == "prod" -o $RUN_MODE == "production" ]; then
+    /usr/local/bin/composer install --no-dev -o --apcu-autoloader
+    /usr/bin/yarn install
+    /usr/bin/yarn run prod
+fi
+
+# Create .env file
+if [ ! -f .env ]; then
+    cp .env.example .env
+fi
+
 # Generate new app key
 /usr/local/bin/php artisan key:generate > /dev/null &
 
@@ -18,15 +30,19 @@ fi
 echo "* Setting up apache"
 
 if [ ! -f /etc/apache2/sites-available/000-laravel.conf ]; then
-    mv /tmp/conf/000-laravel.conf /etc/apache2/sites-available/
-    mv /tmp/conf/001-laravel-ssl.conf /etc/apache2/sites-available/
+    cp /tmp/conf/000-laravel.conf /etc/apache2/sites-available/
+    cp /tmp/conf/001-laravel-ssl.conf /etc/apache2/sites-available/
 fi
 
 /usr/sbin/a2dissite '*'
 /usr/sbin/a2ensite 000-laravel 001-laravel-ssl
 
-# Enable apache rewrite
+# Enable apache mods: rewrite, headers
 a2enmod rewrite
+
+if [ $RUN_MODE == "prod" -o $RUN_MODE == "production" ]; then
+    a2enmod headers
+fi
 
 echo "* Almost ready, starting apache"
 
